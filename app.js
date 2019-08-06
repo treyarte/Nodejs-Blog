224eef955c1d9b36edad9df1536c6e96726d0b5c
Trey <tajohns22@gmail.com>
2019-08-06 11:47:20 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,104 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+
+const postSchema = {
+  title: {
+    type: String,
+    required: [true, "Please add a title to your post."]
+  },
+  content: {
+    type:String,
+    required: [true, "Post content cannot be blank."]
+  }
+};
+
+Post = mongoose.model("Post", postSchema);
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+
+
+app.get("/", function(req, res){
+  Post.find({}, function(err, posts){
+    if(!err){
+      res.render("home", {
+        startingContent: homeStartingContent,
+        posts: posts,
+        _: _ //passing lodash const to make post titles capitalize
+        });
+
+    } else{
+      console.log(err.message);
+    }
+  });
+
+
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: _.lowerCase(req.body.postTitle),
+    content: req.body.postBody
+  });
+  post.save( function(err, documentSaved){
+    if(!err){
+      res.redirect("/");
+      console.log(documentSaved + "\n" + " # of rows affected" );
+    } else {
+      console.log(err.message);
+      res.redirect("/compose");
+    }
+  });
+
+
+
+});
+
+app.get("/posts/:postName", function(req, res){
+  const requestedId = req.params.postName;
+  Post.findOne({_id: requestedId}, function(err, postResult){
+    if(!err){
+      if(postResult){
+        res.render("post", {
+          title: postResult.title,
+          content: postResult.content,
+          _: _ //passing lodash const to make post titles capitalize
+        });
+      }
+    }
+  });
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
